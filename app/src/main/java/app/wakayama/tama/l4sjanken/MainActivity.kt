61014153b8d471f41fa014fb7c56e1462b71830c
MayuWaka <180781056@ccmailg.meijo-u.ac.jp>
2020-09-12 03:55:07 +0900

    じゃんけんアプリ完成（スクール4：9/11）

--- app/src/main/java/app/wakayama/tama/l4sjanken/MainActivity.kt
+++ app/src/main/java/app/wakayama/tama/l4sjanken/MainActivity.kt
@@ -11,75 +11,104 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var kachi: Int = 0
+        var make: Int = 0
+
         gooButton.setOnClickListener {
-            player.text = "あなたの手はグーです"
+            playerImage.setImageResource(R.drawable.goo)
             val number: Int = Random.nextInt(3)
 
             when (number) {
                 0 -> {
-                    cpu.text = "相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo_r)
                     result.text = "引き分けです"
                     result.setTextColor(Color.parseColor("#8a000000"))
                 }
                 1 -> {
-                    cpu.text = "相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki_r)
                     result.text = "あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
+
+                    kachi = kachi + 1
                 }
                 2 -> {
-                    cpu.text = "相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa_r)
                     result.text = "あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
+
+                    make = make + 1
                 }
             }
+
+            // 勝敗を表示する
+            textSyouhai.text = kachi.toString() + " 勝" + make.toString() + " 敗 です。"
+
+//            val ret :Int = showResult()
         }
 
+
         chokiButton.setOnClickListener {
-            player.text = "あなたの手はチョキです"
+            playerImage.setImageResource(R.drawable.choki)
             val number: Int = Random.nextInt(3)
 
             when (number) {
                 0 -> {
-                    cpu.text = "相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo_r)
                     result.text = "あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
 
+                    make = make + 1
                 }
                 1 -> {
-                    cpu.text = "相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki_r)
                     result.text = "引き分けです"
                     result.setTextColor(Color.parseColor("#8a000000"))
                 }
                 2 -> {
-                    cpu.text = "相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa_r)
                     result.text = "あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
+
+                    kachi = kachi + 1
                 }
             }
+            // 勝敗を表示する
+            textSyouhai.text = kachi.toString() + " 勝" + make.toString() + " 敗 です。"
+
         }
 
 
         paaButton.setOnClickListener {
-            player.text = "あなたの手はパーです"
+            playerImage.setImageResource(R.drawable.paa)
+//            player.text = "あなたの手はパーです"
             val number: Int = Random.nextInt(3)
 
             when (number) {
                 0 -> {
-                    cpu.text = "相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo_r)
                     result.text = "あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
+
+                    kachi = kachi + 1
                 }
                 1 -> {
-                    cpu.text = "相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki_r)
                     result.text = "あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
+
+                    make = make + 1
                 }
                 2 -> {
-                    cpu.text = "相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa_r)
                     result.text = "引き分けです"
                     result.setTextColor(Color.parseColor("#8a000000"))
                 }
             }
+
+            // 勝敗を表示する
+            textSyouhai.text = kachi.toString() + " 勝" + make.toString() + " 敗 です。"
+
+//            val ret :Int = showResult()
         }
 
     }
